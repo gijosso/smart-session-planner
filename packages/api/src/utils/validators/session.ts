c969c439929033f28acfd72c862c4047e066f883
Thomas Josso <thomas.josso@gmail.com>
2025-11-19 22:59:21 -0500

    chore(api): fix nullable

--- packages/api/src/utils/validators/session.ts
+++ packages/api/src/utils/validators/session.ts
@@ -121,7 +121,7 @@ export const suggestionInputSchema = z
     (data) => {
       if (data.startDate) {
         const maxDate = new Date();
-        maxDate.setDate(maxDate.getDate() + (data.lookAheadDays ?? 14));
+        maxDate.setDate(maxDate.getDate() + data.lookAheadDays);
         return data.startDate <= maxDate;
       }
       return true;
